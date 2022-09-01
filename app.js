6090891056b8432345afcc772f5e569e20ec98fa
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2022-09-01 07:08:52 -0400

    Initial Upload

---
+++ app.js
@@ -0,0 +1,52 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+//import Babel from "babel-loader";
+//import Polaroid from "./polaroid.js";
+
+
+var name = "Okino Kamali Leiba";
+
+
+class World extends React.Component {
+    render() {
+        return (
+           
+               //<h1 style={{ onClick: "Polaroid" }}>Hello World!</h1>
+               //<h1 onClick={Polaroid}>Hello World!</h1>
+               <h1>Hello World!</h1>
+               //<h2>{new Date().toLocaleTimeString()}</h2>
+          
+        );
+
+    }
+
+}
+
+
+
+
+
+/*
+function Polaroid() {
+    const jsPolaroid = () => {
+        alert("Success");
+    };
+    
+
+    return (
+        <h1 style={{ onClick: "Polaroid" }}>Test</h1>
+    );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<World />);
+*/
+ReactDOM.render(<World />, document.getElementById("root"));
+
+/*
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const h1 = <h1>Hello World!</h1>;
+root.render(h1);
+*/
+
---
+++ polaroid.js
@@ -0,0 +1,27 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+function Polaroid() {
+    /*
+    const jsPolaroid = () => {
+        alert("success");
+        return (
+            <div id="polaroid" className="polaroid">
+                <img src="Images/background_admin.jpg" alt="background" loading="eager" />
+                <div id="text-container" className="text-container">
+                    <p>Memories of Worlds, Hello</p>
+                </div>
+            </div>
+        );
+    };
+    */
+    window.alert("test");
+}
+
+//ReactDOM.render(<Polaroid />, getElementById("polaroid"));
+
+const root = ReactDOM.createRoot(document.getElementById("polaroid"));
+const click_polaroid = <Polaroid />;
+root.render(click_polaroid);
